7d307bd00283cb5f5c7da8d6ee8c61115ca594be
ZhangHNan <a2857327284@163.com>
2020-03-31 22:46:01 +0800

    前端准备

--- src/main/java/wanzhi/gulu/community/dto/AppealDTO.java
+++ src/main/java/wanzhi/gulu/community/dto/AppealDTO.java
@@ -11,4 +11,5 @@ public class AppealDTO {
     private Long gmtCreate;
     private Integer status;
     private ReportDeal deal; //需要其中的targetId、reportCount、banCount、dealResult
+    private String titleShort;    //如果标题或评论的长度大于10：标题前10个字或评论前10个字+。。。小于10直接全部显示
 }
